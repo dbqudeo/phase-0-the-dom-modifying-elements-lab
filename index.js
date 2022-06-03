08b071ef587feaaf24061a8028def156e62e30ca
byeongdae yu <brianbdyu@gmail.com>
2022-06-03 13:34:46 -0400

    commit tree 7cacf82645fea67554416478ed21170b00685531

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "is the champion"
+
+/*
+
+    it("the 'newHeader' variable points to a node 'h1#victory', has \"YOUR-NAME is the champion\" inside", () => {
+      expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+    });
+
+  });
+})
+*/
